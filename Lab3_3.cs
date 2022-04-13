a2d13286457eaadf698d96ec71c2252babbde717
Jim Termini <Jim.Termini@me.com>
2022-04-12 20:36:14 -0400

    Initial Commit

---
+++ Lab3_3.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+public class RestfulServiceClient
+{
+    static HttpClient client = new HttpClient();
+
+    public static async Task<string> Encrypt(string plaintext)
+    {
+        try
+        {
+            var myURI = "https://venus.sod.asu.edu/WSRepository/Services/EncryptionRest/Service.svc/Encrypt?text=" + plaintext;
+
+            HttpResponseMessage response = await client.GetAsync(myURI);
+            response.EnsureSuccessStatusCode();
+            string responseBody = await response.Content.ReadAsStringAsync();
+            // Above three lines can be replaced with new helper method below
+            // string responseBody = await client.GetStringAsync(uri);
+            // Console.WriteLine(responseBody);
+            return responseBody;
+        }
+
+        catch (HttpRequestException e)
+        {
+            Console.WriteLine("\nException Caught!");
+            Console.WriteLine("Message :{0} ", e.Message);
+            return "Your message here";
+        }
+
+    }
+
+    public static async Task<string> Decrypt(string ciphertext)
+    {
+        try
+        {
+            var myURI = "https://venus.sod.asu.edu/WSRepository/Services/EncryptionRest/Service.svc/Decrypt?text=" + ciphertext;
+
+            HttpResponseMessage response = await client.GetAsync(myURI);
+            response.EnsureSuccessStatusCode();
+            string responseBody = await response.Content.ReadAsStringAsync();
+            // Above three lines can be replaced with new helper method below
+            // string responseBody = await client.GetStringAsync(uri);
+            // Console.WriteLine(responseBody);
+            return responseBody;
+        }
+
+        catch (HttpRequestException e)
+        {
+            Console.WriteLine("\nException Caught!");
+            Console.WriteLine("Message :{0} ", e.Message);
+            return "Your message here";
+        }
+
+    }
+
+
+    // public static async Task<string> Fred()
+    // {
+    //     // Call asynchronous network methods in a try/catch block to handle exceptions.
+    //     try
+    //     {
+    //         var myURI = "https://venus.sod.asu.edu/WSRepository/Services/EncryptionRest/Service.svc/Encrypt?text=" + "plaintext";
+
+    //         HttpResponseMessage response = await client.GetAsync(myURI);
+    //         response.EnsureSuccessStatusCode();
+    //         string responseBody = await response.Content.ReadAsStringAsync();
+    //         // Above three lines can be replaced with new helper method below
+    //         // string responseBody = await client.GetStringAsync(uri);
+    //         Console.WriteLine(responseBody);
+    //         return responseBody;
+    //     }
+
+    //     catch (HttpRequestException e)
+    //     {
+    //         Console.WriteLine("\nException Caught!");
+    //         Console.WriteLine("Message :{0} ", e.Message);
+    //         return "Your message here";
+    //     }
+
+    // }
+}
---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿// See https://aka.ms/new-console-template for more information
+class Program {
+    static void Main(string[] args)   {
+        Console.WriteLine("\n\n================================================================================");
+        Console.WriteLine("This is the start of my output!!\n\n");
+
+        var encryptThis = "Goodbye";
+
+        // var myEncryption = RestfulServiceClient.Encrypt(encryptThis).Result.ToString().Replace("\"","");
+        // var myEncryption = RestfulServiceClient.Encrypt(encryptThis).Result.ToString();
+        var myEncryption = RestfulServiceClient.Encrypt(encryptThis).Result;
+
+        Console.WriteLine($"When encrypted ... \"{encryptThis}\" becomes \"{myEncryption.ToString().Replace("\"","")}\"");
+
+        Console.WriteLine();
+
+        // var myDecrypted = RestfulServiceClient.Decrypt(myEncryption).Result.ToString().Replace("\"","");
+        // var myDecrypted = RestfulServiceClient.Decrypt(myEncryption.Replace("\"","")).Result.ToString();
+        var myDecrypted = RestfulServiceClient.Decrypt(myEncryption.Replace("\"","")).Result;
+
+        Console.WriteLine($"When decrypted ... \"{myEncryption.ToString().Replace("\"","")}\" becomes \"{myDecrypted.ToString().Replace("\"","")}\"");
+
+
+        Console.WriteLine("\n\nThis is the end of my output!!");
+        Console.WriteLine("================================================================================\n\n");
+
+
+    }
+}
